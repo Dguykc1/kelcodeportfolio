4d62e3e4a9411e8c6595e0dc903f2dc809f8920e
Dguykc1 <kezechristian@gmail.com>
2022-05-26 10:28:11 +0100

    my portfolio deployment

---
+++ src/main/java/codeblaze/kelcodeportfolio/KelcodeportfolioApplication.java
@@ -0,0 +1,13 @@
+package codeblaze.kelcodeportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelcodeportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelcodeportfolioApplication.class, args);
+	}
+
+}
